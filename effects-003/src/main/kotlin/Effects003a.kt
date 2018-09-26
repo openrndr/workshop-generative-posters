e35bc97dc75e7898fca2edd698f9f5195f268545
Edwin Jakobs <edwin@rndr.studio>
2018-09-26 12:50:32 +0200

    Added effects-003 and folder-monitor-001

---
+++ effects-003/src/main/kotlin/Effects003a.kt
@@ -0,0 +1,51 @@
+import org.openrndr.Program
+
+import org.openrndr.application
+import org.openrndr.configuration
+
+import org.openrndr.color.ColorRGBa
+import org.openrndr.draw.RenderTarget
+import org.openrndr.extensions.Screenshots
+import org.openrndr.math.Vector2
+import org.openrndr.workshop.Separate
+
+class Effects003a : Program() {
+
+    var drawFunc = {}
+    override fun setup() {
+
+        drawFunc = {
+            val scale = (RenderTarget.active.width.toDouble() / width)
+
+            val separate = Separate()
+
+            poster(drawer) {
+                drawer.background(ColorRGBa.BLACK)
+
+                separate.redShift = Vector2(0.1, 0.1)
+                separate.greenShift = Vector2(0.1, 0.0)
+                separate.blueShift = Vector2(0.0, 0.1)
+
+                layer(post = separate) {
+                    drawer.fill = ColorRGBa.WHITE
+                    drawer.stroke = null
+                    drawer.circle(Vector2(width/2.0, height/2.0), 200.0)
+                }
+            }
+        }
+        extend(Screenshots().apply {
+            scale = 4.0
+        })
+    }
+
+    override fun draw() {
+        drawFunc()
+    }
+}
+
+fun main(args: Array<String>) {
+    application(Effects003c(), configuration {
+        width = 480
+        height = 640
+    })
+}
--- filters/src/main/resources/shaders/separate.frag
+++ filters/src/main/resources/shaders/separate.frag
@@ -12,9 +12,9 @@ uniform vec2 greenShift;
 uniform vec2 blueShift;
 
 void main() {
-    float r = texture(tex0, v_texCoord0 + redShift, 0.0).r;
-    float g = texture(tex0, v_texCoord0 + greenShift, 0.0).g;
-    float b = texture(tex0, v_texCoord0 + blueShift, 0.0).b;
-    float a = texture(tex0, v_texCoord0).a;
-    o_output = vec4(r, g, b, a);
+    vec2 ra = texture(tex0, v_texCoord0 + redShift).ra;
+    vec2 ga = texture(tex0, v_texCoord0 + greenShift).ga;
+    vec2 ba = texture(tex0, v_texCoord0 + blueShift).ba;
+    float a = (ra.y + ga.y + ba.y)/3.0;
+    o_output = vec4(ra.x/ra.y, ga.x/ga.y, ba.x/ba.y, 1.0) * a;
 }
